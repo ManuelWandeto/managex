eb3053401826793905637638d743c97fce08a974
Manuel Wandeto <immanuelmojo@hotmail.com>
2024-03-25 14:17:42 +0300

    Update ip_localle.php

--- utils/ip_localle.php
+++ utils/ip_localle.php
@@ -6,13 +6,29 @@ use Monolog\Logger;
 
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
 $dotenv->load();
+function getUserIpAddr() {
+  $ip = '';
+  if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+      // IP from shared internet
+      $ip = $_SERVER['HTTP_CLIENT_IP'];
+  } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+      // IP passed from proxy
+      $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+  } else {
+      // Regular remote address
+      $ip = $_SERVER['REMOTE_ADDR'];
+  }
+  return $ip;
+}
 
-function getIpLocalle(Logger $dbLogger) {
+function getIpLocalle(Logger $logger) {
     $curl = curl_init();
     try {
         $key = $_ENV['GEOAPIFY_KEY'];
+        $clientIp = getUserIpAddr();
+
         curl_setopt_array($curl, array(
-          CURLOPT_URL => "https://api.geoapify.com/v1/ipinfo?apiKey=$key",
+          CURLOPT_URL => $clientIp == '127.0.0.1' || $clientIp == '::1' ? "https://api.geoapify.com/v1/ipinfo?apiKey=$key" : "https://api.geoapify.com/v1/ipinfo?ip=$clientIp&apiKey=$key",
           CURLOPT_RETURNTRANSFER => true,
         ));
         
@@ -22,7 +38,7 @@ function getIpLocalle(Logger $dbLogger) {
         }
         return json_decode($response, true);
       } catch (Exception $e) {
-        $dbLogger->error("Error getting IP localle data", ['message' => $e->getMessage()]);
+        $logger->withName('IP-LOCALLE')->error("Error getting IP localle data", ['message' => $e->getMessage()]);
         throw $e;
       } finally {
         curl_close($curl);
