6847fd0933fcba301f07c9ac1f0c2d4ff8c84651
Manuel Wandeto <immanuelmojo@hotmail.com>
2024-03-25 14:11:47 +0300

    Update process_payment.php

--- controllers/process_payment.php
+++ controllers/process_payment.php
@@ -2,62 +2,79 @@
 require_once(__DIR__ . '/../db/db.inc.php');
 require_once(__DIR__ . '/../db/queries.inc.php');
 require_once(__DIR__ . '/../utils/pesapal_auth.php');
+require_once(__DIR__ .'/../utils/pesapal_status.php');
 require_once(__DIR__ . '/../utils/redirect.php');
-require_once(__DIR__ . '/../utils/redirect.php');
+require_once(__DIR__ . '/../utils/load_env.php');
 require_once(__DIR__ . '/../utils/logger.php');
 
 if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
-$logger->withName('Controller')->info('Process payment request', ['customer' => $_SESSION['customer']['id'], 'plan' => $_SESSION['plan']['id']]);
-
 $trackingId = $_GET['OrderTrackingId'];
+$merchantRef = $_GET['OrderMerchantReference'];
 
-$curl = curl_init();
+$logger->withName('Controller')->info('Process payment request', ['trackingId' => $trackingId, 'merchantRed' => $merchantRef]);
+$paymentType = isset($_GET['type']) ? $_GET['type'] : 'order';
+
 try {
-    $authkey = getAuthKey($logger);
-    // get transaction status
-    curl_setopt_array($curl, [
-        CURLOPT_URL => "https://cybqa.pesapal.com/pesapalv3/api/Transactions/GetTransactionStatus?orderTrackingId=$trackingId",
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_HTTPHEADER => [
-            'Content-Type: application/json',
-            'Accept: application/json',
-            "Authorization: Bearer $authkey"
-        ]
-    ]);
-    $res = json_decode(curl_exec($curl), true);
-    if(!$res || !empty(curl_error($curl))) {
-        throw new Exception("Error getting transation status: " . stripslashes(curl_error($curl)), curl_getinfo($curl, CURLINFO_RESPONSE_CODE));
+    $res = getTransactionStatus($trackingId, $logger);
+
+    if (substr($res['payment_account'], 0, 4) == "4000" && $_ENV['APP_ENV'] === 'development') {
+        // Emulate a card failure
+
+        $res = [
+            "status_code" => 2,
+            "payment_status_description" => "FAILED",
+            "description" => "You have insufficient funds",
+            "payment_method" => "Visa",
+            "payment_account" => "4000XXXXXXXX1018",
+            "currency" => "KES",
+            "amount" => 1250,
+            "created_date" => "2024-02-14 16:54:13"
+        ];
     }
-    if($res['status'] != 200) {
-        throw new Exception("Error getting transation status: " . $res['error']['code'], (int)$res['status']);
-    }
-    // enroll customer to plan paid for, send confirmation email with download link and redirect back to checkout page on step 3 if success
+    
     $status = $res['status_code'];
     $_SESSION['checkout_response'] = $res;
     if($status === 1) {
-        $_SESSION['step'] = 3;
-        // try {
-            //TODO: if enroll_customer fails, retry atleast 3 times
-        // } catch (Exception $e) {
-        //     //throw $th;
-        // }
-        $enrollment = enroll_customer($pdo_conn, [
-            'plan_id' => $_SESSION['plan']['id'],
-            'customer_id' => $_SESSION['customer']['id'],
-            'expiry' => $_SESSION['plan']['payment_frequency'] !== 'ONETIME' ? $_SESSION['plan']['expiry'] : NULL,
-            'amount' => $res['amount']
-        ], $dbLogger);
-        if(!$enrollment) {
-            throw new Exception("Failed to enroll a paid user!", 500);
+        if($paymentType !== 'custom') {
+            $download = register_download($pdo_conn, [
+                "ip" => $_SESSION['localle']['ip'],
+                "country_code" => $_SESSION['localle']['country']['iso_code'],
+                "country_name" => $_SESSION['localle']['country']['name'],
+                "referrer" => $_SESSION['referer'],
+                "customer_id" => $_SESSION['order']['customer'],
+                "plan_id" => $_SESSION['order']['plan'],
+                "order_id" => $_SESSION['order']["id"],
+                "is_paid" => true,
+            ], $dbLogger);
+            
+            $_SESSION['checkout_response'] = array_merge($res, ["download_id" => $download['id']]);
+            complete_order($pdo_conn, $email_conn, [
+                "amount" => $res['amount'],
+                "merchantRef" => $merchantRef,
+                "trackingId" => $trackingId,
+                "download_id" => $download['id']
+            ], $dbLogger);
+        } else {
+            complete_payment_request($pdo_conn, $email_conn, [
+                "merchantRef" => $merchantRef,
+                "trackingId" => $trackingId
+            ], $dbLogger);
         }
-    }
-    redirect("http://localhost:3000/managex/checkout.php?plan=1");
-} catch (Exception $e) {
-    $apiLogger->withName('Controller')->critical("Failed to get transaction status", ['customer' => $_SESSION['customer']['id'], 'plan' => $_SESSION['plan']['id'], 'message' => $e->getMessage()]);
-    redirect("http://localhost:3000/managex/checkout.php?plan=1&status=error");
+        
+        
+        $_SESSION['step'] = 3;
+        $_SESSION['callback_redirect_query']['status'] = "success";
+        redirect($_SESSION['callback_redirect']."?".http_build_query($_SESSION['callback_redirect_query']));
+    } else {
+        $_SESSION['transaction_error'] = $res['description'];
 
-} finally {
-    curl_close($curl);
+        redirect($_SESSION['callback_redirect']."?".http_build_query($_SESSION['callback_redirect_query']));
+    }
+} catch (Exception $e) {
+    $apiLogger->withName('Controller')->critical("Failed to process transation", ['trackingId' => $trackingId, 'merchantRed' => $merchantRef, 'message' => $e->getMessage()]);
+    
+    $_SESSION['transaction_error'] = $e->getMessage();
+    redirect($_SESSION['callback_redirect']."?".http_build_query($_SESSION['callback_redirect_query']));
 }
