815713eb24fa6c3526b13d9c2abc23426f64002f
Manuel Wandeto <immanuelmojo@hotmail.com>
2024-03-25 14:17:49 +0300

    Update logger.php

--- utils/logger.php
+++ utils/logger.php
@@ -1,14 +1,12 @@
 <?php
 use Monolog\Handler\SendGridHandler;
 use Monolog\Processor\WebProcessor;
-require_once(__DIR__ ."/../vendor/autoload.php");
+require_once("load_env.php");
 // use Monolog\Level;
 use Monolog\Formatter\LineFormatter;
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 
-$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__, 1));
-$dotenv->load();
 date_default_timezone_set($_ENV['APP_TIMEZONE']);
 
 $logFileStream = new StreamHandler(__DIR__ . '/../app.log', Logger::DEBUG);
